1df670351b0c4103a1ad65a3b6176bdeb897cedc
Thewildcard-pt <31257166+Thewildcard-pt@users.noreply.github.com>
2020-03-23 14:36:59 +0000

    Menu Prototype
    Added a Main Menu,and some in game prototype ui.

---
+++ Assets/Scripts/Scr_Log_In.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Scr_Log_In : MonoBehaviour
+{
+  
+    public void Change_Scene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+
+}
