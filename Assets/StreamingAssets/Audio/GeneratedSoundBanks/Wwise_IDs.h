621b14a4c0d05f15332dabb2ddefaafba578c3ae
Reinier Vens <rwvens@hotmail.com>
2021-04-29 00:48:25 +0100

    Update Wwise_IDs.h

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -18,9 +18,24 @@ namespace AK
         static const AkUniqueID DROP = 1878686274U;
         static const AkUniqueID EQUIP = 2412608643U;
         static const AkUniqueID PICKUP = 3978245845U;
+        static const AkUniqueID PLAY_GEN_COMBAT_1 = 3953921183U;
+        static const AkUniqueID PLAY_GEN_OW_01 = 3855478249U;
+        static const AkUniqueID PLAY_KAWAII_FUTURE_BASS = 3324019292U;
+        static const AkUniqueID PLAY_MAIN_MENU_01 = 449059865U;
+        static const AkUniqueID PLAY_METAL_BOSS_01 = 826124593U;
+        static const AkUniqueID PLAY_METAL_COMBAT_01 = 2406196U;
+        static const AkUniqueID PLAY_METAL_OW_01 = 1885982954U;
+        static const AkUniqueID RAILGUN_CHARGING = 4073466121U;
         static const AkUniqueID REACTOR_TYPE01 = 108700025U;
         static const AkUniqueID REACTOR_TYPE01_STOP = 1226299660U;
-        static const AkUniqueID TEST_SPASHIP_FULL_UNIT = 937212177U;
+        static const AkUniqueID SENSOR_PING = 2815033830U;
+        static const AkUniqueID STOP_GEN_COMBAT_1 = 13315505U;
+        static const AkUniqueID STOP_GEN_OW_01 = 1938548259U;
+        static const AkUniqueID STOP_KAWAII_FUTURE_BASS = 3844053294U;
+        static const AkUniqueID STOP_MAIN_MENU_01 = 2563154575U;
+        static const AkUniqueID STOP_METAL_BOSS_01 = 2121112483U;
+        static const AkUniqueID STOP_METAL_COMBAT_01 = 1055950810U;
+        static const AkUniqueID STOP_METAL_OW_01 = 3159624164U;
         static const AkUniqueID THRUSTER_LARGE = 2370087034U;
         static const AkUniqueID THRUSTER_LARGE_STOP = 1006509321U;
         static const AkUniqueID THRUSTER_MEDIUM = 2528260676U;
@@ -38,17 +53,34 @@ namespace AK
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID CHARGE_LEVEL = 1574608066U;
         static const AkUniqueID PERFORMANCE_DURABILITY = 965148665U;
         static const AkUniqueID PERFORMANCE_QUALITY = 875134311U;
         static const AkUniqueID PERFORMANCE_THERMAL = 3835443961U;
         static const AkUniqueID REACTOR_LOAD = 3735272724U;
+        static const AkUniqueID SFX_REACTOR_DISTORTION2_DRYWET = 1552130417U;
+        static const AkUniqueID SFX_REACTOR_GRAIN_SPEED = 1663874485U;
+        static const AkUniqueID SFX_REACTOR_PITCH = 12206642U;
+        static const AkUniqueID SFX_SHIP_DRIVE_ELECTRIC_FUZZBOX_FILTER = 2008207144U;
+        static const AkUniqueID SFX_SHIP_DRIVE_NUCLEAR_FILTERDURATION = 255764574U;
+        static const AkUniqueID SFX_SHIP_DRIVE_NUCLEAR_GRAIN_SPEED_MOD = 675480480U;
+        static const AkUniqueID SFX_SHIP_ELECTRIC_SPEED = 2808571831U;
+        static const AkUniqueID SFX_SHIP_ROTOR_PLATES_GRAIN_FILTER = 1345552351U;
+        static const AkUniqueID SFX_SHIP_WEAPON_LIGHT_DISHI_PITCH = 1246165564U;
+        static const AkUniqueID SFX_SHIP_WEAPON_LIGHTING_GRAIN_FILTER = 4216139142U;
+        static const AkUniqueID SFX_SHIP_WEAPON_LIGHTING_PITCH = 1027880318U;
         static const AkUniqueID SHIP_VELOCITY = 3579545711U;
+        static const AkUniqueID TARGET_LOCKING = 3288537160U;
         static const AkUniqueID THRUSTER_THROTTLE = 4116566921U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BOSS = 1560169506U;
+        static const AkUniqueID COMBAT = 2764240573U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID OVERWORLD = 1562068129U;
         static const AkUniqueID SHIP = 284967655U;
         static const AkUniqueID UI = 1551306167U;
     } // namespace BANKS
